934af83d7e2930cf3331d521c2d67e7ca23e230d
M.Fahad Naseem <64665585+iFahadNaseem@users.noreply.github.com>
2023-04-04 11:31:24 +0500

    initial commit

---
+++ src/api/section-type/controllers/section-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * section-type controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::section-type.section-type');
---
+++ src/api/section-type/routes/section-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * section-type router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::section-type.section-type');
